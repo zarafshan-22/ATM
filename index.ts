5d89c7f734ea2a20f1177344adf7ff551be72a40
zarafshan-22 <zarafshan365@gmail.com>
2024-03-27 19:46:50 +0500

    Created ATM Tool

---
+++ index.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let mybalance: number = 10000;
+let pin: number = 12345;
+let isConfirm: boolean = false;
+
+let pinAnswer = await inquirer.prompt([{
+    name: "pin",
+    message: "Enter Your Pin Code",
+    type: "number"
+}]);
+
+if (pinAnswer.pin === pin) {
+    console.log("You Entered the Correct Pin Code!");
+    isConfirm = true;
+} else {
+    console.log(`You Entered the Wrong Pin Code: ${pinAnswer.pin}`);
+}
+
+if (isConfirm) {
+    let operatorAnswer = await inquirer.prompt([{
+        name: "operator",
+        message: "Select One Options",
+        type: "list",
+        choices: ["Withdraw", "Check Balance", "FastCash"]
+    }]);
+
+    if (operatorAnswer.operator === "Withdraw") {
+        let amountAnswer = await inquirer.prompt([{
+            name: "amount",
+            message: "Enter The Amount You Want To Withdraw",
+            type: "number",
+        }]);
+
+        if (amountAnswer.amount <= mybalance) {
+            mybalance -= amountAnswer.amount;
+            console.log(`Now Your Balance Is ${mybalance}`);
+        } else {
+            console.log("Insufficient Balance");
+        }
+    } else if (operatorAnswer.operator === "Check Balance") {
+        console.log(`Your current balance is ${mybalance}`);
+    } else if (operatorAnswer.operator === "FastCash") {
+        let PriceAnswer = await inquirer.prompt([{
+            name: "amount",
+            message: "Please Select Amount",
+            type: "list",
+            choices: ["5000", "3000", "2000", "1000"]
+        }])
+        if (PriceAnswer.amount <= mybalance) {
+            mybalance -= PriceAnswer.amount;
+            console.log(`Now Your Balance Is ${mybalance}`);
+        }
+    }
+} else {
+    console.log("Your Pin Code Is Incorrect");
+}
